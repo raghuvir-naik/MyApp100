a9114f5a81bc2355706e727ed70708f3decdde9d
RAGNAIK <RAGNAIK@RAGNAIK-IN.in.oracle.com>
2021-02-22 19:11:56 +0900

    converted into maven project. and tested it. commit1

---
+++ src/com/ofss/TestProduct.java
@@ -0,0 +1,14 @@
+package com.ofss;
+
+public class TestProduct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stock stk = new Stock(100,"ORCL",3500.43);
+		
+		System.out.println(stk);
+
+	}
+
+}

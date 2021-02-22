bb95bcdc163deeb3cb59beaa0e9e6eed975113e6
RAGNAIK <RAGNAIK@RAGNAIK-IN.in.oracle.com>
2021-02-22 19:33:28 +0900

    Added Customer Java class

--- src/com/ofss/TestProduct.java
+++ src/com/ofss/TestProduct.java
@@ -5,9 +5,13 @@ public class TestProduct {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Stock stk = new Stock(100,"ORCL",3500.43);
+		Stock stk[]= new Stock[10];
+		 stk[0] = new Stock(100,"ORCL",3500.43);
+		 stk[1] = new Stock(200,"IBM",435.43);
 		
-		System.out.println(stk);
+		Customer cust = new Customer("Raghu", stk);
+		
+		System.out.println(cust);
 
 	}
 

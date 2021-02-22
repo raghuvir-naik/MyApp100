0ecabba61df4ec15b0eb7398bb3d11a0bfbf1031
RAGNAIK <RAGNAIK@RAGNAIK-IN.in.oracle.com>
2021-02-22 20:03:55 +0900

    New feature to test

--- src/com/ofss/TestProduct.java
+++ src/com/ofss/TestProduct.java
@@ -9,10 +9,28 @@ public class TestProduct {
 //		 stk[0] = new Stock(100,"ORCL",3500.43);
 //		 stk[1] = new Stock(200,"IBM",435.43);
 		
-		 Stock stk[]= {new Stock(100,"ORCL",3500.43),new Stock(200,"IBM",435.43)};
+		Stock stk[]= {new Stock(100,"ORCL",3500.43),new Stock(200,"IBM",435.43)};
 		Customer cust = new Customer("Raghu", stk);
 		
 		System.out.println(cust);
+		
+		double maxPrice=0.0;
+		String maxPricedStock="";
+		
+		// Let's use for loop
+		for (int i=0;i<stk.length;i++)
+		{
+			if (stk[i].getStockPrice()>maxPrice)
+			{
+				maxPrice=stk[i].getStockPrice();
+				maxPricedStock=stk[i].getStockName();
+			}
+			
+		}
+		
+		System.out.println("The stock name with the highest price is "+maxPricedStock+" and price is "+maxPrice);
+
+		
 
 	}
 

aa24f0d2d84bc16685198bd5d1249b4e887d4eab
RAGNAIK <RAGNAIK@RAGNAIK-IN.in.oracle.com>
2021-02-22 19:42:58 +0900

    Without arraysize

--- src/com/ofss/TestProduct.java
+++ src/com/ofss/TestProduct.java
@@ -5,10 +5,11 @@ public class TestProduct {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Stock stk[]= new Stock[10];
-		 stk[0] = new Stock(100,"ORCL",3500.43);
-		 stk[1] = new Stock(200,"IBM",435.43);
+		//Stock stk[]= new Stock[2];
+//		 stk[0] = new Stock(100,"ORCL",3500.43);
+//		 stk[1] = new Stock(200,"IBM",435.43);
 		
+		 Stock stk[]= {new Stock(100,"ORCL",3500.43),new Stock(200,"IBM",435.43)};
 		Customer cust = new Customer("Raghu", stk);
 		
 		System.out.println(cust);
